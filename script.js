3823f7b110b955ea8b30e9aa5bb6242a32cb3c64
ivanlandry <58294595+ivanlandry@users.noreply.github.com>
2024-07-06 02:38:34 -0400

    remise en page

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var url = window.location.href;
-document.querySelectorAll('nav a').forEach(function (element){
-    if (element.href===url){
-        element.classList.add('active');
-    }
+document.querySelectorAll('nav a').forEach(function (element,index) {
+    if(index>0)
+        element.classList.remove('active');
 });
+
+function removeActive(el) {
+    document.querySelectorAll('nav a').forEach(function (element,index) {
+        if(element.classList.contains('active'))
+            element.classList.remove('active');
+    });
+    el.classList.add('active');
+};
+
